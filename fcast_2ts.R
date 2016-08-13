8641096ce0e19aadd58e37b76520b137c1f2dddf
Alejandro Rojas <alejandrorojas@tests-MacBook-Pro.local>
2016-08-12 23:03:26 -0400

    forecast

---
+++ fcast_2ts.R
@@ -0,0 +1,19 @@
+################################################################
+# Directory   : /Users/alejandrorojas/data/lab3_for_students_final
+# Dataset: ex3series.csv
+# Program Name: fcast_2ts.R
+# Analyst     : Alejandro Rojas
+# Last Updated: 8/9/2016 
+################################################################
+# -------------------------------------------------------------------
+# Main Topics Covered:
+#
+# Part 1 (50 points): Forecast the Web Search Activity for global Warming
+#The file, google_correlate_flight.csv, contains the relative web search activity for the phrase “flight prices” over time. 
+#Your data science team believes that search activity for this phrase is positively correlated with consumer demand for flying (and possibly prices). 
+#Your task is to forecast the relative demand of this phrase for the year 2016. 
+#For the purposes of this assignment, ignore the units of the data as they are not relevant here.
+#Remember to explain and justify each of your modelling decisions. 
+#Also, comment on your forecast. 
+#Do you notice anything interesting? Do you notice anything worth worrying about?
+#####################################################################
